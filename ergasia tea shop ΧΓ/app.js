12d4caae8f28ede6fc26288d5a1e9b923a57965f
Chrysoula Gkipatoudi <goldusqueen@gmail.com>
2025-01-17 16:50:58 +0200

    Add files via upload

---
+++ ergasia tea shop ΧΓ/app.js
@@ -0,0 +1,11 @@
+const date = (document.getElementById('date').innerHTML =
+    new Date().getFullYear())
+const navBtn = document.getElementById('nav-btn')
+const navbar = document.getElementById('navbar')
+const navClose = document.getElementById('nav-close')
+navBtn.addEventListener('click', () => {
+    navbar.classList.add('showNav')
+})
+navClose.addEventListener('click', () => {
+    navbar.classList.remove('showNav')
+})
